2bc57cb51a9754bccb17fe66993070aeacca5c6b
Andrea Simoncini <andrea.simoncini@tangible.is>
2025-10-01 14:05:11 +0200

    creating components

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import { socialLinks } from '../data'
+import PageLinks from './PageLinks'
+
+const Footer = () => {
+	return (
+		<footer className='section footer'>
+			<PageLinks parentClass='footer-links' itemClass='footer-link' />
+
+			<ul className='footer-icons'>
+				{socialLinks.map((link) => {
+					const { id, url, icon } = link
+					return (
+						<li key={id}>
+							<a
+								href={url}
+								target='_blank'
+								rel='noreferrer'
+								className='footer-icon'>
+								<i className={icon}></i>
+							</a>
+						</li>
+					)
+				})}
+			</ul>
+
+			<p className='copyright'>
+				copyright &copy; Backroads travel tours company
+				<span id='date'>{new Date().getFullYear()}</span> all rights reserved
+			</p>
+		</footer>
+	)
+}
+export default Footer
